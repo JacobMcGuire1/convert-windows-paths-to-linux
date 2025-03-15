1faa04df3978b099a1dfdee1ac0179df7af5e932
mcguirejacob1 <mcguirejacob1@gmail.com>
2025-03-15 16:49:46 +0000

    Escape paths

--- tmf.ps1
+++ tmf.ps1
@@ -1,6 +1,10 @@
 $windowsPath = $PWD.Path
 $pythonScript = Join-Path -Path $PSScriptRoot -ChildPath "convert.py"
 $linuxPath = python $pythonScript $windowsPath
+
 Write-Output "Local Path: $windowsPath"
 Write-Output "Linux Path: $linuxPath"
-ssh -t jacob@192.168.1.193 "cd '$linuxPath'; bash --login; tmf"
+
+$escapedLinuxPath = $linuxPath -replace "'", "'\''"
+
+ssh -t jacob@192.168.1.193 "cd '$escapedLinuxPath'; bash -i -c 'tmf'"
